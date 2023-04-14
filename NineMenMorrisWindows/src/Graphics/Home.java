a0f97cc86a7f4396698170b5b5c4221783db0d25
IlayBor <ilaybor2004@gmail.com>
2023-04-14 19:17:59 +0300

    change button layout

--- NineMenMorrisWindows/src/Graphics/Home.java
+++ NineMenMorrisWindows/src/Graphics/Home.java
@@ -16,7 +16,7 @@ import org.w3c.dom.Text;
 
 public class Home extends JPanel implements ActionListener{
 	private Frame frame;
-	private JButton localButton, AiButton;
+	private JButton localButton, AiButton, settingsButton;
 	private ImageIcon image;
 	
 	public Home(Frame _frame) 
@@ -27,14 +27,19 @@ public class Home extends JPanel implements ActionListener{
 		image = new ImageIcon("images/BackgroundImage.jpeg");
 		
 		localButton = new JButton("Play Local");
-		localButton.setBounds(400,300,200,50);
+		localButton.setBounds(400,275,200,50);
 		localButton.addActionListener(this);
 		add(localButton);
 		
 		AiButton = new JButton("Play Ai");
-		AiButton.setBounds(400,400,200,50);
+		AiButton.setBounds(400,375,200,50);
 		AiButton.addActionListener(this);
 		add(AiButton);
+		
+		settingsButton = new JButton("Settings");
+		settingsButton.setBounds(400,475,200,50);
+		settingsButton.addActionListener(this);
+		add(settingsButton);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
